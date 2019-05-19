a1ec09f6b910dbb85dc4718835362d067f962db7
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-19 13:01:15 -0500

    added empty LoginActivity and RegisterFragment

---
+++ app/src/main/java/com/isra/israel/travelmem/fragment/RegisterFragment.java
@@ -0,0 +1,27 @@
+package com.isra.israel.travelmem.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.isra.israel.travelmem.R;
+
+public class RegisterFragment extends Fragment {
+
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+}
