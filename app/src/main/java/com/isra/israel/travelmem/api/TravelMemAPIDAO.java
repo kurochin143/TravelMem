51d233988a9e167438f30ac3e3f14fe059fd7219
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-20 10:36:40 -0500

    added methods to TravelMemAPIInterface

--- app/src/main/java/com/isra/israel/travelmem/api/TravelMemAPIDAO.java
+++ app/src/main/java/com/isra/israel/travelmem/api/TravelMemAPIDAO.java
@@ -1,12 +1,24 @@
 package com.isra.israel.travelmem.api;
 
-import com.isra.israel.travelmem.gson.TravelMemGson;
+import android.util.Pair;
 
+import com.isra.israel.travelmem.gson.TravelMemGson;
+import com.isra.israel.travelmem.model.Travel;
+
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public class TravelMemAPIDAO {
 
@@ -29,10 +41,23 @@ public class TravelMemAPIDAO {
             .addConverterFactory(GsonConverterFactory.create(TravelMemGson.gson))
             .client(okHttpClient)
             .build();
-    public static TravelMemFirebaseAPIInterface apiService = retrofit.create(TravelMemFirebaseAPIInterface.class);
+    public static TravelMemAPIInterface apiService = retrofit.create(TravelMemAPIInterface.class);
 
-    public interface TravelMemFirebaseAPIInterface {
+    public interface TravelMemAPIInterface {
+        @GET(JSON)
+        Call<ResponseBody> verifyIdToken(@Query(value = "auth") String idToken);
 
+        @GET(TRAVELS + "{uid}/" + JSON)
+        Call<HashMap<String, Travel>> getTravels(@Path(value = "uid") String uid, @Query(value = "auth") String idToken);
+
+        @POST(TRAVELS + "{uid}/" + JSON)
+        Call<Pair<String, String>> addTravel(@Path(value = "uid") String uid, @Query(value = "auth") String idToken, @Body Travel travel);
+
+        @PUT(TRAVELS + "{uid}/{travelId}" + JSON)
+        Call<ResponseBody> updateTravel(@Path(value = "uid") String uid, @Path(value = "travelId") String travelId, @Query(value = "auth") String idToken, @Body Travel travel);
+
+        @GET(USERS + "{uid}/" + JSON)
+        Call<ResponseBody> getUser(@Path(value = "uid") String uid, @Query(value = "auth") String idToken);
     }
 
 }
